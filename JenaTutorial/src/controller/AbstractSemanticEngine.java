036dbaaa747beb1217ec3ef15ff89c87ed9256cc
Felipe_Noguez <Felipe_Noguez@RBSN0863.RBS.NET>
2013-12-10 16:58:37 -0200

    - Inicio da programação do tratamento das listas de palavras - Geração de query para pesquisa com base nas palavras

--- JenaTutorial/src/controller/AbstractSemanticEngine.java
+++ JenaTutorial/src/controller/AbstractSemanticEngine.java
@@ -1,6 +1,9 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.text.Segment;
 
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryFactory;
@@ -42,8 +45,67 @@ public abstract class AbstractSemanticEngine {
 	 * Trata o conteudo informado no campo de busca para posterior pesquisa 
 	 * @return	Verdadeiro se procedimento terminou corretamente
 	 */
-	public Boolean preparaConteudoBusca() {
+	public boolean preparaConteudoBusca() {
 		if (!this.textoBusca.isEmpty()){
+			
+			String[] lstPalavras =  this.textoBusca.split(" ");
+			int posPrimeiraReservada = 0;
+			int posSegundaReservada = 0;
+			List<String> palavrasDaPesquisa = new ArrayList<String>();
+			
+			//Remove as palavras reservadas - as que não entram na pesquisa
+			for (String palavra: lstPalavras){
+				if (!ehPalavraReservada(palavra)){
+					palavrasDaPesquisa.add(palavra);					
+				}
+			}
+			lstPalavras = (String[]) palavrasDaPesquisa.toArray();
+			int contador = 0;
+			//
+			for (String palavra: lstPalavras){
+				if (ehPalavraLigacao(palavra)){
+					//Ao encontrar a palavra marca a posicao inicial se não existir
+					if (posPrimeiraReservada == 0){ posPrimeiraReservada = contador; }
+					//Senão 
+					else {
+						String palavraPesquisa = "";
+						for (int i = posPrimeiraReservada; i < posSegundaReservada; i++){
+							//Concatenando as palavras do intervalo
+							palavraPesquisa += " " + lstPalavras[i];
+						}
+						//Nova palavra vai pra lista de palavras
+						palavrasDaPesquisa.add(palavraPesquisa);
+						//Reseta ponteiros 
+						posPrimeiraReservada = 0;
+						posSegundaReservada = 0;
+					}
+				}else{
+					
+				}
+				contador++;
+			}
+			
+			
+			
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean ehPalavraLigacao(String palavra){
+		if ( "em".equals(palavra) || 
+				"na".equals(palavra) || 
+				"no".equals(palavra) || 
+				"de".equals(palavra) || 
+				"com".equals(palavra)){
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean ehPalavraReservada(String palavra){
+		if ( "bairro".equals(palavra) ||
+				"cidade".equals(palavra)){
 			return true;
 		}
 		return false;
--- JenaTutorial/src/controller/SemanticEngineImoveis.java
+++ JenaTutorial/src/controller/SemanticEngineImoveis.java
@@ -3,13 +3,31 @@ package controller;
 import java.util.List;
 
 import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
 
 
 public class SemanticEngineImoveis extends AbstractSemanticEngine {
+	
+	private static final String PREFIX_IMOVEIS = "PREFIX ont: <http://www.semanticweb.org/felipe/ontologies/2013/11/untitled-ontology-131#>";
 
 	@Override
 	protected Query geraConsultaSPARQL(List<String> lstPalavras) {
-		// TODO Auto-generated method stub
+		if (lstPalavras.size() >0){
+			//Configura o prefixo da ontologia de imoveis
+			String queryString= PREFIX_IMOVEIS;
+			
+			//Monta a querystring com as palavras informadas
+			queryString += "SELECT distinct ?parametro WHERE { ?x ?c ?y. ?x ont:Nome_Parametro ?parametro FILTER(";
+			boolean primeiraPalavra = true;
+			for(String palavra: lstPalavras){
+				if (!primeiraPalavra) queryString += "||";
+				queryString += "regex(?y, \" " + palavra + " \", \"i\")";
+			}
+			queryString += ")}";
+			
+			//Cria a query
+			return QueryFactory.create(queryString);
+		}
 		return null;
 	}
 
