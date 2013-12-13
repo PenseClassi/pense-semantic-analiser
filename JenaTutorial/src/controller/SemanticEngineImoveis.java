f49ba36d996b5bf15c4055392a79a2836678bba2
Felipe_Noguez <Felipe_Noguez@RBSN0863.RBS.NET>
2013-12-13 11:28:00 -0200

    Fix

--- JenaTutorial/src/controller/SemanticEngineImoveis.java
+++ JenaTutorial/src/controller/SemanticEngineImoveis.java
@@ -2,15 +2,27 @@ package controller;
 
 import java.util.List;
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.shared.JenaException;
 
 
 public class SemanticEngineImoveis extends AbstractSemanticEngine {
 	
 	private static final String PREFIX_IMOVEIS = "PREFIX ont: <http://www.semanticweb.org/felipe/ontologies/2013/11/untitled-ontology-131#>";
 
+	public SemanticEngineImoveis(){
+		setOntologia(ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null));
+		try {
+		    getOntologia().read("file:./src/owl/Imoveis.owl", "RDF/XML");
+	    }catch (JenaException je) {        
+
+        }
+	}
+	
 	@Override
 	protected Query geraConsultaSPARQL(List<String> lstPalavras) {
 		if (lstPalavras.size() >0){
@@ -22,7 +34,8 @@ public class SemanticEngineImoveis extends AbstractSemanticEngine {
 			boolean primeiraPalavra = true;
 			for(String palavra: lstPalavras){
 				if (!primeiraPalavra) queryString += "||";
-				queryString += "regex(?y, \"^ " + palavra + " $\", \"i\")";
+				queryString += "regex(?y, \"" + palavra + "\", \"i\")";
+//				queryString += "regex(?y, \"" + palavra + "\", \"i\")";
 				primeiraPalavra = false;
 			}
 			queryString += ")}";
